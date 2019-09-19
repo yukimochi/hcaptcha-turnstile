53e1207a060e7426c47a2efc68071dc6ae68bb98
Tyler VanNurden <tyler.vannurden@colorado.edu>
2019-09-18 19:13:43 -0600

    Confirm a working POC

--- lib/hcaptcha/adapters/controller_methods.rb
+++ lib/hcaptcha/adapters/controller_methods.rb
@@ -7,6 +7,55 @@ module Hcaptcha
 
       # Your private API can be specified in the +options+ hash or preferably
       # using the Configuration.
+      def verify_hcaptcha(options = {})
+        options = {model: options} unless options.is_a? Hash
+        return true if Hcaptcha.skip_env?(options[:env])
+
+        model = options[:model]
+        attribute = options.fetch(:attribute, :base)
+        recaptcha_response = options[:response] || recaptcha_response_token(options[:action])
+
+        begin
+          verified = if Hcaptcha.invalid_response?(recaptcha_response)
+            false
+          else
+            unless options[:skip_remote_ip]
+              remoteip = (request.respond_to?(:remote_ip) && request.remote_ip) || (env && env['REMOTE_ADDR'])
+              options = options.merge(remote_ip: remoteip.to_s) if remoteip
+            end
+
+            Hcaptcha.verify_via_api_call(recaptcha_response, options)
+          end
+
+          if verified
+            flash.delete(:recaptcha_error) if recaptcha_flash_supported? && !model
+            true
+          else
+            recaptcha_error(
+              model,
+              attribute,
+              options.fetch(:message) { Hcaptcha::Helpers.to_error_message(:verification_failed) }
+            )
+            false
+          end
+        rescue Timeout::Error
+          if Hcaptcha.configuration.handle_timeouts_gracefully
+            recaptcha_error(
+              model,
+              attribute,
+              options.fetch(:message) { Hcaptcha::Helpers.to_error_message(:recaptcha_unreachable) }
+            )
+            false
+          else
+            raise HcaptchaError, 'Hcaptcha unreachable.'
+          end
+        rescue StandardError => e
+          raise HcaptchaError, e.message, e.backtrace
+        end
+      end
+
+      # Your private API can be specified in the +options+ hash or preferably
+      # using the Configuration.
       def verify_recaptcha(options = {})
         options = {model: options} unless options.is_a? Hash
         return true if Hcaptcha.skip_env?(options[:env])
