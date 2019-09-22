987119c0837114cd4b3d799f8a6157383b1442b5
Tyler VanNurden <tyler.vannurden@colorado.edu>
2019-09-22 08:35:56 -0600

    Remove test keys

--- demo/sinatra/server.rb
+++ demo/sinatra/server.rb
@@ -6,8 +6,8 @@ set :bind, 'test.mydomain.com'
 set :port, 3000
 
 Hcaptcha.configure do |config|
-  config.site_key  = '1161d0be-1130-4af5-8999-b6fa8894e2a8'
-  config.secret_key = '0x2c87B9A9B41a6112FEC89dd16C138DDEe62D9472'
+  config.site_key  = 'abc'
+  config.secret_key = '123'
 end
 
 include Hcaptcha::Adapters::ControllerMethods
